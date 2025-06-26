1c73c5d5d6d4e4f046a09cef6efd245f35352f04
Wayne917917 <a0921284620@gmail.com>
2025-06-26 12:10:10 +0800

    add ds_01.java

---
+++ 0626/ds_01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ds_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入第一個整數: ");
+        int x = scanner.nextInt();
+        System.out.print("請輸入第二個整數: ");
+        int  y = scanner.nextInt();
+        int sum = x + y;
+        System.out.println("兩個整數的和為: " + sum);
+        scanner.close();
+    }
+}
