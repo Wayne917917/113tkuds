5ffb45cacd1e282c8fba004c934f16ce4e07f89a
Wayne917917 <a0921284620@gmail.com>
2025-06-29 03:39:21 +0800

    add ds_07.java

--- 0626/ds_07.java
+++ 0626/ds_07.java
@@ -5,7 +5,6 @@ public class ds_07 {
             {4, 5, 6},
             {7, 8, 9}
         };
-
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(arr[i][j] + " ");
