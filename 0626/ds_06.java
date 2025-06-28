82bf6a61d24487a18adaf7c06e817df1cd130c92
Wayne917917 <a0921284620@gmail.com>
2025-06-29 03:37:24 +0800

    add ds_06.java

---
+++ 0626/ds_06.java
@@ -0,0 +1,14 @@
+public class ds_06 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("最大值是：" + max);
+    }
+}
