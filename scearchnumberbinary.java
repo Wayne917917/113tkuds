4d80af2d529e5a7528cf27c65cbed02314e36e63
Wayne917917 <a0921284620@gmail.com>
2025-07-02 04:20:49 +0800

    add arraysum

---
+++ scearchnumberbinary.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class scearchnumberbinary {
+    public static void main(String[] args) {
+        int[] s = {1, 3, 5, 7, 9, 11, 13};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入要查找的數字：");
+        int x = scanner.nextInt();
+
+        int left = 0, right = s.length - 1;
+        boolean found = false;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (s[mid] == x) {
+                found = true;
+                break;
+            } else if (s[mid] < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        if (found) {
+            System.out.println(Yes);
+        } else {
+            System.out.println(No);
+        }
+    }
+}
